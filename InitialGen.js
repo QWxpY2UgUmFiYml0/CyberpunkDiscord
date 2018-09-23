c5a1ce686663d76a7458b3456875710530fc53aa
Unknown <QWxpY2UgUmFiYml0@gmail.com>
2018-09-23 13:10:48 -0400

    Formatting

--- InitialGen.js
+++ InitialGen.js
@@ -5,8 +5,8 @@
 var alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'];
 
 
-function roll(min, max){//Tested
-    var rand = Math.floor(Math.random()*(max-min+1)+min);
+function roll(min, max) { //Tested
+    var rand = Math.floor(Math.random() * (max - min + 1) + min);
     return rand;
 }
 
@@ -14,77 +14,86 @@ function roll(min, max){//Tested
 //Gain 4 spaces of memory for each CPU, one Code Gate, and one Terminal
 var cpuCount = roll(1, 6);
 
-var personalities = ["Friendly/Curious", "Hostile/Paranoid", 
-                    "Stable/Intelligent/Buisnesslike", "Intellectual/Detached",
-                     "Machinelike", "Remote/Godlike"];
+var personalities = ["Friendly/Curious", "Hostile/Paranoid",
+    "Stable/Intelligent/Buisnesslike", "Intellectual/Detached",
+    "Machinelike", "Remote/Godlike"
+];
 
 var reaction = ["Neutral", "Neutral", "Kill all intruders",
-                "Observe intruders, then act", "Report all intruders", 
-                "Talk to intruder to find intent"];
+    "Observe intruders, then act", "Report all intruders",
+    "Talk to intruder to find intent"
+];
 
 var icon = ["Human", "Geometric", "Mythological",
-            "Voice only", "Technic", "\"Humanoid\""];
+    "Voice only", "Technic", "\"Humanoid\""
+];
 
 var fileType = ["Inter Office", "Database", "Buisness Records",
-                "Financial Transactions", "Grey Ops", "Black Ops"];
+    "Financial Transactions", "Grey Ops", "Black Ops"
+];
 
 var virtuals = ["Virtual Conference", "Virtual Office", "Virtual Rec-Area",
-                "Virtual Building", "Virtual City", "Virtual World"];
+    "Virtual Building", "Virtual City", "Virtual World"
+];
 
-var realism = ["Simple", "Simple", "Contextual", 
-            "Fractal", "Photorealistic", "Superrealistic"];
+var realism = ["Simple", "Simple", "Contextual",
+    "Fractal", "Photorealistic", "Superrealistic"
+];
 
 var detectionAlarm = ["Watchdog", "Watchdog", "Bloodhound", "Bloodhound",
-                      "Pitbull", "Pitbull"];
+    "Pitbull", "Pitbull"
+];
 
-var antiIC = ["Killer", "Killer", "Manticore", "Manticore", "Aardvark", "Aardvark"]; 
+var antiIC = ["Killer", "Killer", "Manticore", "Manticore", "Aardvark", "Aardvark"];
 
 var antiSystem = ["Flatline", "Poison Flatline", "Krash", "Viral 15",
-                  "DecKrash", "Murphy"];
+    "DecKrash", "Murphy"
+];
 
 var antiPersonnel = ["Stun", "Hellbolt", "Brainwipe", "Knockout", "Zombie", "Hellhound"];
 
 var defType = [detectionAlarm, detectionAlarm, detectionAlarm, detectionAlarm,
     antiIC, antiIC, antiSystem, antiSystem,
-     antiPersonnel, antiPersonnel];
+    antiPersonnel, antiPersonnel
+];
 
-function findWallStr(){//Tested
+function findWallStr() { //Tested
     var rand = roll(1, 6);
-    var str = Math.floor(rand/2 + cpuCount);
+    var str = Math.floor(rand / 2 + cpuCount);
     return str;
 }
 
-function findSkillRanks(){//Tested
+function findSkillRanks() { //Tested
     var arr = [];
-    for(var i = 0; i < 5; i++){
-        arr.push(roll(1, 6) + 4 )
+    for (var i = 0; i < 5; i++) {
+        arr.push(roll(1, 6) + 4)
     }
     return arr;
 }
 
-function findFileType(){//Tested
+function findFileType() { //Tested
     var rand = roll(0, 5);
-    return fileType[rand];//Need to place in memory loc of my choice
+    return fileType[rand]; //Need to place in memory loc of my choice
 }
 
-function findVirtuals(){
+function findVirtuals() {
     var rand = roll(0, 5);
     return virtuals[rand];
 }
 
-function findRealism(){
+function findRealism() {
     var rand = roll(0, 5);
     return realism[rand];
 }
 
-function findDefenses(){
+function findDefenses() {
     var defenses = [];
     var defCount = roll(1, 6) + cpuCount;
-    
-    for(var i = 0; i < defCount; i++){
+
+    for (var i = 0; i < defCount; i++) {
         var rand = roll(0, 9);
         defenses.push(defType[rand][roll(0, 5)]);
-        
+
     }
     console.log(defenses);
     return defenses;
@@ -97,40 +106,39 @@ function createTable() {
     var html = '';
     var defenses = findDefenses();
     var done = false;
-    
+
     // Assume data is an array of arrays,
     // containing strings.
 
     html += '<table class="data">';
-    
+
     // Step through the rows of the data.
-    for(var x = 0; x<16; x++) {
+    for (var x = 0; x < 16; x++) {
         //var rowData = data[row];
-        html+= '<tr>'
-        
+        html += '<tr>'
+
         // Step through the columns in
         // this row.
-        for(var i =0; i<16; i++) {
-            html += '<td class="cell" id="'+alpha[x]+i+'">';
-                //here is where we will add the code to import the Cyberdeck Programs
-                if(i<defenses.length && done == false){
-                    //id will resemble a0-c for row[a] col[0]. The -c is to differentiate
-                    //between table cell and content.
-                    html += '<img id="'+alpha[x]+i+'-c'+'" alt="'+defenses[i]+'" src="token.png" draggable="true" ondragstart="drag(event)">';
-                }
-                else{
-                    //html += '<img id="'+alpha[x]+i+'" src="dataWall.png">'; //fills with walls
-                    done = true;
-                }
-                
+        for (var i = 0; i < 16; i++) {
+            html += '<td class="cell" id="' + alpha[x] + i + '">';
+            //here is where we will add the code to import the Cyberdeck Programs
+            if (i < defenses.length && done == false) {
+                //id will resemble a0-c for row[a] col[0]. The -c is to differentiate
+                //between table cell and content.
+                html += '<img id="' + alpha[x] + i + '-c' + '" alt="' + defenses[i] + '" src="token.png" draggable="true" ondragstart="drag(event)">';
+            } else {
+                //html += '<img id="'+alpha[x]+i+'" src="dataWall.png">'; //fills with walls
+                done = true;
+            }
+
             html += '</td>';
         }
-        
+
         html += '</tr>';
     }
-    
+
     html += '</table>';
-    
+
     return html;
 }
 
@@ -139,18 +147,18 @@ function createTable() {
 // 'child' HTML of the specified element.
 function insertHTML(id, html) {
     var el = document.getElementById(id);
-    
-    if(!el) {
+
+    if (!el) {
         alert('Element with id ' + id + ' not found.');
     }
-    
+
     el.innerHTML = html;
 }
 
 // This function ties things up.
 function run() {
     var html = createTable();
-    
+
     insertHTML('tabledata', html);
 }
 
